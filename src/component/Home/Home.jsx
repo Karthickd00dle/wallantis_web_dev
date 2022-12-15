77bb6ca5ead4380ac80655c3c308caaad9ef4779
Praveen Murugan <pmurugan@vaeso.com>
2022-12-15 18:32:23 +0530

    Card one WIP

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./styles.scss";
+import wallpaperPng from "../../assets/images/wallpaperCard.png";
+import HomeCardOne from "./subcomponents/CardOne";
 
 export const HomeComponentMain = () => {
   return (
     <>
-      <div className="">Helloo</div>
+      <div className="home-content-container">
+        <div className="home-wall-mural">
+          <div className="home-wall-mural--title">WALLANTICS</div>
+          <div className="home-wall-mural--title-two">
+            An Stylish and most beautiful Wallpapers{" "}
+          </div>
+
+          <div className="card-one-container">
+            <div className="card-one-inner-container">
+              <HomeCardOne wallpaperPng={wallpaperPng} />
+              <HomeCardOne wallpaperPng={wallpaperPng} />
+              <HomeCardOne wallpaperPng={wallpaperPng} />
+            </div>
+          </div>
+        </div>
+
+        <div className="why-wallantics-container">
+          <div className="why-wallantics--title">Why WALLANTICS ?</div>
+
+          <div className="why-wallantics-card-container">
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 };
