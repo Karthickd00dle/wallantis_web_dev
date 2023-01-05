319dd31f4b742f9073c50559cbf7cb732b7327e8
Praveen M <praveen.02@doodleblue.com>
2023-01-05 11:41:44 +0530

    WIP

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,13 +1,11 @@
 import { Home } from "component/Home/Home";
-import ProductDetailComponent from "component/ProductDetail";
 import React, { Component } from "react";
 // Redux Connection
 import { connect } from "react-redux";
 
 export class HomePageClass extends Component {
   render() {
-    // return <Home />;
-    return <ProductDetailComponent/>
+    return <Home />;
   }
 }
 export const HomePage = connect(null, null)(HomePageClass);
