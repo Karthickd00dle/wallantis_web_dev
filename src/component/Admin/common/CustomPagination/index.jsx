2cdf9ac752ad827aa90a031551bfe620fcd5d3f3
Yuvaraja M <yuvaraja.m@doodleblue.com>
2023-01-27 13:19:28 +0530

    pagination fixes and inventory management complete

--- src/component/Admin/common/CustomPagination/index.jsx
+++ src/component/Admin/common/CustomPagination/index.jsx
@@ -1,11 +1,22 @@
 import * as React from "react";
+import { styled } from "@mui/material/styles";
 import Pagination from "@mui/material/Pagination";
 import "./style.scss";
+
+const StyledPagination = styled((props) => (
+  <Pagination disableRipple {...props} />
+))(() => ({
+  "& .MuiButtonBase-root.Mui-selected": {
+    backgroundColor: "#FFFFFF",
+    color: "#1890FF",
+    borderColor: "#1890FF",
+  },
+}));
 
 export default function CustomPagination({ pageCount, currentPage, onChange }) {
   return (
     <div className="d-flex justify-content-center">
-      <Pagination
+      <StyledPagination
         className="pagination-style"
         count={pageCount}
         variant="outlined"
