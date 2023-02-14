40fa0ba93611b351fc8af3e39bb2abd6d7e69b76
Nishant <nishants440@gmail.com>
2023-02-14 11:25:01 +0530

    Login API Fixed

--- src/service/api.js
+++ src/service/api.js
@@ -54,7 +54,6 @@ let getServiceUrl = (baseURL) => {
   let finalURL = "";
   const { REACT_APP_AUTH_API_BASE_URL, REACT_APP_USER_URL, REACT_APP_KIM_URL } =
     process.env;
-  console.log(REACT_APP_AUTH_API_BASE_URL);
   switch (baseURL) {
     case "auth":
       finalURL = `${REACT_APP_AUTH_API_BASE_URL}/api/v1/`;
