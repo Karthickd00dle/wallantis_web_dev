48235d4c0e28c416c8f29253b24c08cfe6a6b818
Nishant <nishants440@gmail.com>
2023-02-20 13:03:32 +0530

    Fixes

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { CustomHeader } from "component/common";
 import { NormalFooter } from "component/common/Footer";
+import { ChatBot } from "component/common/ChatBot";
 import "assets/scss/layouts/HomeLayout.scss";
 
 export function HomeLayout({ children }) {
   return (
     <>
+      <div>
+        <ChatBot />
+      </div>
       <CustomHeader />
       <div className="container-layout">{children}</div>
       <NormalFooter />
