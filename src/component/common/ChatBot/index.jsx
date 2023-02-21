343e05f3547dd28920bb530909e96368b9fc3818
Nishant <nishants440@gmail.com>
2023-02-21 15:12:35 +0530

    Responsiveness added

--- src/component/common/ChatBot/index.jsx
+++ src/component/common/ChatBot/index.jsx
@@ -6,53 +6,8 @@ export function ChatBot() {
   return (
     <div>
       <div className="chatbot-btn">
-        <img src={chatIcon} alt="chatIcon" />
+        <img className="chatbot-image" src={chatIcon} alt="chatIcon" />
       </div>
-      {/* <div className="Bot">
-        <div className="Askexperts">
-          <label>Ask Our Experts</label>
-          <br />
-          <span>Welcome to Wallantics</span>
-          <br />
-          <small>
-            We are here to help you with our service related queries.
-          </small>
-        </div>
-        <div className="Botchoose">
-          <label>Choose an option to know more about our Products</label>
-        </div>
-        <div className="choosecontents1">
-          <div className="choosewallpaper">Wallpapers</div>
-          <div className="choosestikerwallpaper">
-            <label>Sticker Wallpaper</label>
-          </div>
-          <div className="choosewallmurals">
-            <label>Wall Murals</label>
-          </div>
-        </div>
-
-        <div className="choosecontents2">
-          <div className="chooseorderqueries">
-            <label>Order Related Queries</label>
-          </div>
-          <div className="chooserelatedqueries">
-            <label>Payment Related Queries</label>
-          </div>
-        </div>
-
-        <div className="choosecontents3">
-          <div className="choosebookings">
-            <label>Bookings</label>
-          </div>
-          <div className="chooseinstaller">
-            <label>Installer Related Queries </label>
-          </div>
-        </div>
-        <hr></hr>
-        <div className="botconversation">
-          <input type="text" placeholder="Start a converstation" />
-        </div>
-      </div> */}
     </div>
   );
 }
