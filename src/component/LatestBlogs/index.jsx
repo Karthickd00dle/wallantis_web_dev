b41b25a210ff0f0c65c3934028273efbca115f3c
dj_0512 <dhanuja.v@doodleblue.in>
2023-10-27 18:20:56 +0530

    Blogs UI fix

--- src/component/LatestBlogs/index.jsx
+++ src/component/LatestBlogs/index.jsx
@@ -2,10 +2,65 @@ import React from "react";
 import { history } from "service/helpers";
 import "./style.scss";
 import { NormalNavigate } from "component/common";
-
+import blog1 from "../../assets/images/Blogs/Blog1.png";
+import blog2 from "../../assets/images/Blogs/Blog3.png";
+import blog3 from "../../assets/images/Blogs/Blog4.png";
+import blog4 from "../../assets/images/Blogs/Blog5.png";
+import blog5 from "../../assets/images/Blogs/Blog6.png";
+import blog6 from "../../assets/images/Blogs/Blog7.png";
 import JohnDoe from "assets/images/JohnDoe";
 
 export default function Latestblogs() {
+  const latestBlogs = [
+    {
+      blogImage: blog1,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "How to choose wallpapers for renovating homes....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+    {
+      blogImage: blog2,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "Wallcoverings spotted in the world....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+    {
+      blogImage: blog3,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "Top 5 best traditional design wallpapers....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+    {
+      blogImage: blog4,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "5 ways to choose wallpapers for your Home....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+    {
+      blogImage: blog5,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "10 Great wallpaper designs for Kids Nursery Rooms....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+    {
+      blogImage: blog6,
+      name: "John Doe",
+      blogDate: "14 Oct'2022",
+      blogTitle: "Ideas to transform your Kitchen with wallpapers....",
+      blogContent:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in turpis libero odio varius proin lectus. Nulla cras pharetra viverra amet turpis netus. Ipsum sem quis in cursus. Massa vitae, ut eget fermentum ut nunc.",
+    },
+  ];
   return (
     <div>
       <div>
@@ -15,221 +70,30 @@ export default function Latestblogs() {
           </div>
         </NormalNavigate>
       </div>
-      <div>
-        <div className="division1">
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog1"></div>
-            <div className="blog1contents">
-              <div className="blog1img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct'2022</span>
+      <div className="division1">
+        {latestBlogs.map((item) => {
+          return (
+            <div
+              onClick={() => history.push("/profile/blogdetails")}
+              className="cursor-pointer"
+            >
+              <img src={item?.blogImage} className="blogImage" />
+              <div className="blogContents">
+                <div className="blog1img">
+                  <JohnDoe className="johndoe" />
+                  <div className="john">
+                    <h6>{item?.name}</h6>
+                    <span>{item?.blogDate}</span>
+                  </div>
                 </div>
+                <label>{item?.blogTitle}</label>
+                <h5>{item?.blogContent}</h5>
               </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <br />
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
             </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog2"></div>
-            <div className="blog2contents">
-              <div className="blog2img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog3"></div>
-            <div className="blog3contents">
-              <div className="blog3img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-        </div>
-
-        <div className="division2">
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog4"></div>
-            <div className="blog4contents">
-              <div className="blog4img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog5"></div>
-            <div className="blog5contents">
-              <div className="blog5img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog6"></div>
-            <div className="blog6contents">
-              <div className="blog6img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-        </div>
-
-        <div className="division3">
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog7"></div>
-            <div className="blog7contents">
-              <div className="blog7img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog8"></div>
-            <div className="blog8contents">
-              <div className="blog8img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-
-          <div
-            className="blog-main"
-            onClick={() => history.push("/profile/blogdetails")}
-          >
-            <div className="latestblog9"></div>
-            <div className="blog9contents">
-              <div className="blog9img">
-                <JohnDoe className="johndoe" />
-                <div className="john">
-                  <h6>John Doe </h6>
-                  <span>14 Oct’2022</span>
-                </div>
-              </div>
-              <label>How to choose wallpapers for renovating homes....</label>
-              <h5>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id in
-                turpis libero odio varius proin lectus. Nulla cras pharetra
-                viverra amet turpis netus. Ipsum sem quis in cursus. Massa
-                vitae, ut eget fermentum ut nunc.{" "}
-              </h5>
-            </div>
-          </div>
-        </div>
-
+          );
+        })}
+      </div>
+      <div className="loadMoreWrap">
         <button className="loadmore">Load More</button>
       </div>
     </div>
